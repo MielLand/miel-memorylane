76501e8b6b39c606f252b8bb64c56150a1923083
MielLand <101825661+MielLand@users.noreply.github.com>
2025-11-04 15:16:42 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 -- General
 Config.Cooldown = 60 -- Seconds between memories
-Config.Chance = 20 -- meaning 10 percent chance it happens once player entered radius
+Config.Chance = 20 -- meaning xx percent chance it happens once player entered radius
 -- Default random lines
 Config.DefaultMessages = {
     "You feel a strange sense of nostalgia...",
@@ -19,4 +19,5 @@ Config.MemorySpots = {
     {coords = vector3(1854.1, 3683.5, 34.2), radius = 6.0, message = "The desert whispers back your first arrest..."},
     {coords = vector3(440.8, -981.9, 30.6), radius = 6.0, message = "You recall stepping into the PD for the very first time."}
 }
+
 
